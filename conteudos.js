4e3c1add74da1b88b3bfed537b413cb7c6d6d97a
ugangelo <gabriel.angelos@gmail.com>
2026-08-08 09:14:22 -0300

    teste
    teste

---
+++ conteudos.js
@@ -0,0 +1,460 @@
+window.conteudosAulas = {
+    "2026-08-13": {
+        "titulo": [
+            "Introdução e conceitos básicos",
+            "Apresentação"
+        ],
+        "itens": [
+            {
+                "capitulo": "1",
+                "conteudo": "INTRODUÇÃO E CONCEITOS BÁSICOS",
+                "pagina": "1"
+            },
+            {
+                "capitulo": "1-1",
+                "conteudo": "TERMODINÂMICA E TRANSFERÊNCIA DE CALOR",
+                "pagina": "2"
+            },
+            {
+                "capitulo": "1-3",
+                "conteudo": "CALOR E OUTRAS FORMAS DE ENERGIA",
+                "pagina": "6"
+            },
+            {
+                "capitulo": "",
+                "conteudo": "BREVE APRESENTAÇÃO DA DISCIPLINA",
+                "pagina": ""
+            },
+            {
+                "capitulo": "1-3.1",
+                "conteudo": "C. ESPECÍFICO DE GASES, LÍQUIDOS E SÓLIDOS",
+                "pagina": "7"
+            },
+            {
+                "capitulo": "1-4",
+                "conteudo": "A PRIMEIRA LEI DA TERMODINÂMICA",
+                "pagina": "11"
+            }
+        ]
+    },
+    "2026-08-11": {
+        "titulo": "Condução",
+        "itens": [
+            {
+                "capitulo": "1-5",
+                "conteudo": "MECANISMOS DE TRANSFERÊNCIA DE CALOR",
+                "pagina": "17"
+            },
+            {
+                "capitulo": "1-6",
+                "conteudo": "CONDUÇÃO",
+                "pagina": "17"
+            },
+            {
+                "capitulo": "1-6.1",
+                "conteudo": "CONDUTIVIDADE TÉRMICA",
+                "pagina": "19"
+            },
+            {
+                "capitulo": "1-6.2",
+                "conteudo": "DIFUSIVIDADE TÉRMICA",
+                "pagina": "23"
+            }
+        ]
+    },
+    "2026-08-18": {
+        "titulo": "Resistência térmica",
+        "itens": [
+            {
+                "capitulo": "1-3.2",
+                "conteudo": "TRANSFERÊNCIA DE ENERGIA",
+                "pagina": "9"
+            },
+            {
+                "capitulo": "3",
+                "conteudo": "CONDUÇÃO DE CALOR PERMANENTE",
+                "pagina": "131"
+            },
+            {
+                "capitulo": "3-1",
+                "conteudo": "COND. DE CALOR P. EM PAREDES PLANAS",
+                "pagina": "132"
+            },
+            {
+                "capitulo": "3-1.1",
+                "conteudo": "CONCEITO DE RESISTÊNCIA TÉRMICA",
+                "pagina": "133"
+            }
+        ]
+    },
+    "2026-08-20": {
+        "titulo": "Convecção",
+        "itens": [
+            {
+                "capitulo": "1-7",
+                "conteudo": "CONVECÇÃO",
+                "pagina": "25"
+            },
+            {
+                "capitulo": "",
+                "conteudo": "FIGURA 1-32",
+                "pagina": "26"
+            },
+            {
+                "capitulo": "",
+                "conteudo": "FIGURA 1-33",
+                "pagina": "26"
+            },
+            {
+                "capitulo": "",
+                "conteudo": "TABELA 1-5",
+                "pagina": "26"
+            }
+        ]
+    },
+    "2026-08-25": {
+        "titulo": "Condução axial",
+        "itens": [
+            {
+                "capitulo": "3-2",
+                "conteudo": "RESISTÊNCIA DE CONTATO",
+                "pagina": "142"
+            },
+            {
+                "capitulo": "[EXP]",
+                "conteudo": "EXPERIMENTO DE CONDUÇÃO AXIAL",
+                "pagina": "[EXP]"
+            }
+        ]
+    },
+    "2026-08-27": {
+        "titulo": "Radiação",
+        "itens": [
+            {
+                "capitulo": "1-8",
+                "conteudo": "RADIAÇÃO",
+                "pagina": "27"
+            },
+            {
+                "capitulo": "1-8",
+                "conteudo": "TABELA 1-6 - EMISSIVIDADE",
+                "pagina": "28"
+            },
+            {
+                "capitulo": "1-8",
+                "conteudo": "EXEMPLO 1-9",
+                "pagina": "29"
+            }
+        ]
+    },
+    "2026-09-01": {
+        "titulo": "Convecção e radiação",
+        "itens": [
+            {
+                "capitulo": "[EXP]",
+                "conteudo": "CONV. E RAD. EM PARALELO",
+                "pagina": "[EXP]"
+            }
+        ]
+    },
+    "2026-09-03": {
+        "titulo": "Resistência térmica generalizada",
+        "itens": [
+            {
+                "capitulo": "3-1.2",
+                "conteudo": "REDE DE RESISTÊNCIA TÉRMICA",
+                "pagina": "135"
+            },
+            {
+                "capitulo": "3-1.3",
+                "conteudo": "PAREDES PLANAS MULTICAMADAS",
+                "pagina": "137"
+            },
+            {
+                "capitulo": "3-3",
+                "conteudo": "RESISTÊNCIA TÉRMICA GENERALIZADA",
+                "pagina": "147"
+            }
+        ]
+    },
+    "2026-09-08": {
+        "titulo": "Cilindros e esferas",
+        "itens": [
+            {
+                "capitulo": "3-4",
+                "conteudo": "COND. DE CALOR EM CILINDROS E ESFERAS",
+                "pagina": "150"
+            }
+        ]
+    },
+    "2026-09-17": {
+        "titulo": "Exercícios",
+        "conteudo": "Exercícios"
+    },
+    "2026-09-15": {
+        "titulo": "Exercícios",
+        "conteudo": "Exercícios"
+    },
+    "2026-10-06": {
+        "titulo": "Raio crítico de isolamento",
+        "itens": [
+            {
+                "capitulo": "3-5",
+                "conteudo": "RAIO CRÍTICO DE ISOLAMENTO",
+                "pagina": "156"
+            }
+        ]
+    },
+    "2026-09-10": {
+        "titulo": "Equação da condução",
+        "itens": [
+            {
+                "capitulo": "2",
+                "conteudo": "EQUAÇÃO DA CONDUÇÃO DE CALOR",
+                "pagina": "61"
+            },
+            {
+                "capitulo": "2-1",
+                "conteudo": "INTRODUÇÃO",
+                "pagina": "62"
+            },
+            {
+                "capitulo": "2-3",
+                "conteudo": "EQUAÇÃO GERAL DE CONDUÇÃO DE CALOR",
+                "pagina": "74"
+            }
+        ]
+    },
+    "2026-10-08": {
+        "titulo": "Condução transiente",
+        "itens": [
+            {
+                "capitulo": "4",
+                "conteudo": "CONDUÇÃO DE CALOR TRANSIENTE",
+                "pagina": "217"
+            },
+            {
+                "capitulo": "4-1",
+                "conteudo": "ANÁLISE DE SISTEMAS CONCENTRADOS",
+                "pagina": "218"
+            },
+            {
+                "capitulo": "4-1.1",
+                "conteudo": "CRITÉRIOS ... SISTEMAS CONCENTRADOS",
+                "pagina": "219"
+            },
+            {
+                "capitulo": "4-1.2",
+                "conteudo": "ALGUMAS OBSERVAÇÕES SOBRE A TRANSF.",
+                "pagina": "221"
+            }
+        ]
+    },
+    "2026-10-27": {
+        "titulo": "Trocadores de calor",
+        "itens": [
+            {
+                "capitulo": "11",
+                "conteudo": "TROCADORES DE CALOR",
+                "pagina": "609"
+            },
+            {
+                "capitulo": "11-1",
+                "conteudo": "TIPOS DE TROCADORES DE CALOR",
+                "pagina": "610"
+            },
+            {
+                "capitulo": "11-2",
+                "conteudo": "O COEFICIENTE GLOBAL DE T.C.",
+                "pagina": "613"
+            },
+            {
+                "capitulo": "11-3",
+                "conteudo": "ANÁLISE DE TROCADORES DE CALOR",
+                "pagina": "620"
+            },
+            {
+                "capitulo": "11-4",
+                "conteudo": "MÉTODO DA DIFERENÇA MÉDIA LOG.",
+                "pagina": "622"
+            }
+        ]
+    },
+    "2026-10-15": {
+        "titulo": "Superfícies aletadas",
+        "itens": [
+            {
+                "capitulo": "3-6",
+                "conteudo": "T.C. A PARTIR DE SUPERFÍCIES ALETADAS",
+                "pagina": "159"
+            },
+            {
+                "capitulo": "3-6.1",
+                "conteudo": "EQUAÇÃO DA ALETA",
+                "pagina": "160"
+            },
+            {
+                "capitulo": "3-6",
+                "conteudo": "TRANSF. SUPERFÍCIES ALETADAS",
+                "pagina": "159"
+            }
+        ]
+    },
+    "2026-10-20": {
+        "titulo": "Eficiência de aletas",
+        "itens": [
+            {
+                "capitulo": "3-6.2",
+                "conteudo": "EFICIÊNCIA DE UMA ALETA",
+                "pagina": "164"
+            },
+            {
+                "capitulo": "",
+                "conteudo": "FIGURA 3-42",
+                "pagina": "167"
+            },
+            {
+                "capitulo": "",
+                "conteudo": "FIGURA 3-43",
+                "pagina": "167"
+            },
+            {
+                "capitulo": "[EXP]",
+                "conteudo": "EXPERIMENTO DE ALETAS",
+                "pagina": "[EXP]"
+            }
+        ]
+    },
+    "2026-10-22": {
+        "titulo": "Fundamentos da convecção",
+        "itens": [
+            {
+                "capitulo": "6",
+                "conteudo": "FUNDAMENTOS DA CONVECÇÃO",
+                "pagina": "355"
+            },
+            {
+                "capitulo": "6-1",
+                "conteudo": "MECANISMO FÍSICO DA CONVECÇÃO",
+                "pagina": "356"
+            },
+            {
+                "capitulo": "6-1.1",
+                "conteudo": "NÚMERO DE NUSSELT",
+                "pagina": "358"
+            },
+            {
+                "capitulo": "6-3",
+                "conteudo": "CAMADA LIMITE HIDRODINÂMICA",
+                "pagina": "362"
+            },
+            {
+                "capitulo": "6-4",
+                "conteudo": "CAMADA LIMITE TÉRMICA",
+                "pagina": "364"
+            },
+            {
+                "capitulo": "6-4.1",
+                "conteudo": "NÚMERO DE PRANDTL",
+                "pagina": "365"
+            },
+            {
+                "capitulo": "6-5.1",
+                "conteudo": "NÚMERO DE REYNOLDS",
+                "pagina": "366"
+            },
+            {
+                "capitulo": "7",
+                "conteudo": "CONVECÇÃO FORÇADA EXTERNA",
+                "pagina": "395"
+            },
+            {
+                "capitulo": "7-2",
+                "conteudo": "ESC. PARALELO SOBRE PLACAS PLANAS",
+                "pagina": "399"
+            }
+        ]
+    },
+    "2026-10-29": {
+        "titulo": "Escoamento sobre cilindros e esferas",
+        "itens": [
+            {
+                "capitulo": "7-3",
+                "conteudo": "ESCOAMENTO SOBRE CILINDROS E ESFERAS",
+                "pagina": "408"
+            },
+            {
+                "capitulo": "7-3.2",
+                "conteudo": "COEF. DE TRANSFERÊNCIA DE CALOR",
+                "pagina": "412"
+            }
+        ]
+    },
+    "2026-11-03": {
+        "titulo": "Exercícios",
+        "conteudo": "Exercícios"
+    },
+    "2026-11-05": {
+        "titulo": "Convecção interna",
+        "itens": [
+            {
+                "capitulo": "8",
+                "conteudo": "CONVECÇÃO FORÇADA INTERNA",
+                "pagina": "451"
+            },
+            {
+                "capitulo": "8-1",
+                "conteudo": "INTRODUÇÃO",
+                "pagina": "452"
+            },
+            {
+                "capitulo": "8-3",
+                "conteudo": "A REGIÃO DE ENTRADA",
+                "pagina": "455"
+            },
+            {
+                "capitulo": "8-3.1",
+                "conteudo": "COMPRIMENTOS DE ENTRADA",
+                "pagina": "457"
+            },
+            {
+                "capitulo": "8-4",
+                "conteudo": "ANÁLISE TÉRMICA GERAL",
+                "pagina": "458"
+            },
+            {
+                "capitulo": "8-4.1",
+                "conteudo": "FLUXO DE CALOR CTE NA SUPERFÍCIE",
+                "pagina": "459"
+            },
+            {
+                "capitulo": "8-4.2",
+                "conteudo": "TEMPERATURA CTE DA SUPERFÍCIE",
+                "pagina": "460"
+            }
+        ]
+    },
+    "2026-11-10": {
+        "titulo": "Efetividade e NTU",
+        "itens": [
+            {
+                "capitulo": "11-5",
+                "conteudo": "MÉTODO DA EFETIVIDADE E NTU",
+                "pagina": "631"
+            },
+            {
+                "capitulo": "",
+                "conteudo": "FIGURA 11-26",
+                "pagina": "637"
+            }
+        ]
+    },
+    "2026-11-12": {
+        "titulo": "Exercícios",
+        "conteudo": "Exercícios"
+    },
+    "2026-11-17": {
+        "titulo": "Backup, ajustes, exercícios e dúvidas",
+        "conteudo": "Backup, ajustes, exercícios e dúvidas"
+    }
+};
